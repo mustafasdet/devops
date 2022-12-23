711a37b17114c39bd7020bf155c55febd6ae16ed
mustafa-idward <mustafa.cevik@id-ward.com>
2022-12-23 16:21:37 +0300

    master-copy2 1st commit

--- git.py
+++ git.py
@@ -2,8 +2,11 @@ print("Hello git")
 
 print("second commit")
 
-print ("test branch")
+print("test branch")
 
-print ("test branch again")
+print("test branch again")
 
 print("master's line-5th -we changed it to 9th line")
+
+print("Change in copy ==> master-copy")
+print("Change in copy ==> master-copy")
