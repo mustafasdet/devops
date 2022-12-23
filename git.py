dca058190f646ccdf2bf023e2600d9c169f98d6d
mustafasdet <94309681+mustafasdet@users.noreply.github.com>
2022-12-23 18:29:52 +0300

    Update git.py

--- git.py
+++ git.py
@@ -19,9 +19,3 @@ print ("Master master master master")
 print("Change in copy ==> master-copy")
 print("Change in copy ==> master-copy")
 print ("Master master master master")
-
-print("Change in copy ==> master-copy")
-print("Change in copy ==> master-copy")
-
-print("Change in copy ==> master-copy")
-print("Change in copy ==> master-copy")
